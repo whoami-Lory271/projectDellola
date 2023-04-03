897be35d8c0ee34b169d31d630db1e57870be10d
Lorenzo De Santis <lorenzo.desantis@igt.com>
2023-04-03 11:38:23 +0200

    Auto-generated commit [ci skip]

--- src/app/Dog.ts
+++
@@ -1,7 +0,0 @@
-export interface Dog {
-  id: number;
-  dog_name: string,
-  description: string;
-  cute_level: number;
-  src: string;
-}
--- src/app/mock-dogs.ts
+++
@@ -1,9 +0,0 @@
-import {Dog} from "./Dog";
-
-export  const DOGS: Dog[] = [
-  {id: 1, dog_name: 'happy_della', description: 'A cute happy doggo', cute_level: 100000, src: "/assets/images/msg71844346-106779.jpg"},
-  {id: 2, dog_name: 'sad_della', description: 'A cute sad doggo', cute_level: 10000001, src: "/assets/images/msg71844346-106780.jpg"},
-  {id: 3, dog_name: 'angry_della', description: 'A cute angry doggo', cute_level: 100000001, src: "/assets/images/msg71844346-106781.jpg"},
-  {id: 3, dog_name: 'hungry_della', description: 'A cute hungry doggo', cute_level: 999999999, src: "/assets/images/msg71844346-106782.jpg"},
-  {id: 4, dog_name: 'cute_della', description: 'A cute cute doggo', cute_level: 999999999, src: "/assets/images/msg71844346-106778.jpg"}
-];
